50e6cf24b5c5b36d6976948df5f7e3078db8514c
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-05-13 21:12:08 +0200

    fixed

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,17 +2,17 @@ import css from "./MovieList.module.css";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
-export default function MovieList({ movies }) {
-  const location = useLocation();
-  return (
-    <ul>
-      {movies.map((item) => (
-        <li key={item.id} className={css.item}>
-          <Link to={`/movies/${item.id}`} state={location} className={css.link}>
-            {item.title}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-}
+// export default function MovieList({ movies }) {
+//   const location = useLocation();
+//   return (
+//     <ul>
+//       {movies.map((item) => (
+//         <li key={item.id} className={css.item}>
+//           <Link to={`/movies/${item.id}`} state={location} className={css.link}>
+//             {item.title}
+//           </Link>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
